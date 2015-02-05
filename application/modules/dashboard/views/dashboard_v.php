a75f63616236543baa23faa9f947cc5b5746fdd5
MwangiKevin <mwangikevinn@gmail.com>
2015-02-05 16:37:14 +0300

    halfway there

--- application/modules/dashboard/views/dashboard_v.php
+++ application/modules/dashboard/views/dashboard_v.php
@@ -1,1 +1,63 @@
-<div style="height:20000px">Dashboard stub</div>
+<div style="height:20000px">
+
+	<div class="accordion" id="accordion2">
+		<div class="accordion-group panel panel-default">
+			<div class="accordion-heading panel-heading">Testing Trends
+				<a class="accordion-toggle collapsed right" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+					Click To expand
+				</a>
+			</div>
+			<div id="collapseOne" class="accordion-body panel-body collapse" style="height: 0px;">
+				<div class="accordion-inner">
+
+					<div class="panel panel-default">
+						<div class="panel-heading">Bar Chart</div>
+						<div class="panel-body">
+							<div class="chart-container"><canvas id="bar" class="chart chart-bar ng-isolate-scope" data="data" labels="labels" series="series" height="261" width="523" style="width: 523px; height: 261px;"></canvas></div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+
+		<div class="accordion-group panel panel-default">
+			<div class="accordion-heading panel-heading">Facilities
+				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+					 Click To expand
+				</a>
+			</div>
+			<div id="collapseTwo" class="accordion-body collapse panel-body">
+				<div class="accordion-inner">
+				</div>
+			</div>
+		</div>
+
+
+		<div class="accordion-group panel panel-default">
+			<div class="accordion-heading panel-heading">Lab Performance
+				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse-lab">
+					 Click To expand
+				</a>
+			</div>
+			<div id="collapse-lab" class="accordion-body collapse panel-body">
+				<div class="accordion-inner">
+				</div>
+			</div>
+		</div>
+
+
+		<div class="accordion-group panel panel-default">
+			<div class="accordion-heading panel-heading">TB Co-Infection 
+				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+					Click To expand
+				</a>
+			</div>
+			<div id="collapseThree" class="accordion-body collapse panel-body">
+				<div class="accordion-inner">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
