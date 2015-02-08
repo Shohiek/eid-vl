f78b27fd457c75e9701b9ad5aef2a47d88849db0
MwangiKevin <mwangikevinn@gmail.com>
2015-02-08 21:59:35 +0300

    bound filter components

--- application/modules/filters/views/filter_view.php
+++ application/modules/filters/views/filter_view.php
@@ -32,42 +32,36 @@ $('#reportrange').daterangepicker(
    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')],
    'Last Three Months': [moment().subtract('month', 3).startOf('month'), moment().subtract('month').endOf('month')],
    'Last Six Months': [moment().subtract('month', 6).startOf('month'), moment().subtract('month').endOf('month')],
+   'This Year': [moment().startOf('year'),moment()],
  },
- startDate: moment().subtract('days', 29),
+ startDate: moment().startOf('year'),
  endDate: moment(),
  showWeekNumbers:true,
  showDropdowns:true
 },
 function(start, end) {
   $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-  $('#fro').val(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY')).trigger('change');
-  $('#to').val(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY')).trigger('change');
+  $('#fro').val(start.format('YYYY-MM-D') ).trigger('change');
+  $('#to').val(end.format('YYYY-MM-D') ).trigger('change');
 }
 );
 </script>
 
 <div class="row " style="border-bottom: 1px solid #eeeeee;padding-bottom:10px">
   <div class="col-md-3 filterSection">
-    <ui-select ng-model="address.selected" theme="bootstrap" ng-disabled="disabled" reset-search-input="false" style="" class="filterItem">
-    <ui-select-match placeholder="Select A Program...">{{$select.selected.formatted_address}}</ui-select-match>
-    <ui-select-choices repeat="address in addresses track by $index" refresh="refreshAddresses($select.search)"  refresh-delay="0">
-    <div ng-bind-html="address.formatted_address | highlight: $select.search"></div>
+    <ui-select ng-model="filters.programs.selected " theme="bootstrap" ng-disabled="disabled" reset-search-input="false" style="" class="filterItem">
+    <ui-select-match placeholder="Select a Program...">{{$select.selected.name +" ("+ $select.selected.initials +")" }}</ui-select-match>
+    <ui-select-choices repeat="program in filters.programs track by $index" >
+    <div ng-bind-html="program.name | highlight: $select.search"></div>
   </ui-select-choices>
 </ui-select>
 </div>
 
 <div class="col-md-3 filterSection">
 
-<!-- <ui-select ng-model="address.selected" theme="bootstrap" ng-disabled="disabled" reset-search-input="false" style="" class="filterItem">
-<ui-select-match placeholder="Search Criteria to Filter by...">{{$select.selected.formatted_address}}</ui-select-match>
-<ui-select-choices repeat="address in addresses track by $index" refresh="refreshAddresses($select.search)"  refresh-delay="0">
-<div ng-bind-html="address.formatted_address | highlight: $select.search"></div>
-</ui-select-choices>
-</ui-select> -->
-
-  <ui-select ng-model="person.selected" theme="bootstrap" ng-disabled="disabled" style="min-width: 300px;">
+  <ui-select ng-model="filters.entities.selected" theme="bootstrap" ng-disabled="disabled" style="min-width: 300px;">
     <ui-select-match placeholder="Search Criteria to Filter by...">{{$select.selected.name +" ("+ $select.selected.type +")"  }}</ui-select-match>
-    <ui-select-choices group-by="'type'" repeat="entity in filterentities | propsFilter: {name: $select.search, phone: $select.search}">
+    <ui-select-choices group-by="'type'" repeat="entity in filters.entities | entityFilter: {name: $select.search, phone: $select.search}">
       <div ng-bind-html="entity.name | highlight: $select.search"></div>
       <small>
         email: {{entity.email}}
@@ -81,7 +75,7 @@ function(start, end) {
 
 <div class="input-group" class="filterItem">
   <div class="input-group-btn ">
-    <button type="button" class="btn btn-primary filterButton"><i class="fa fa-undo fa-sm"></i> Reset</button>      
+    <button type="button" ng-click="clear()" class="btn btn-primary filterButton"><i class="fa fa-undo fa-sm"></i> Reset</button>      
   </div>
 </div>
 </div>
@@ -91,7 +85,7 @@ function(start, end) {
     <div class="input-group-btn " >
       <div id="reportrange" class="btn btn-primary pull-right filterButton" style="width: 100%;">
         <i class="fa fa-calendar fa-md"></i>
-        <span><?php echo date("F j, Y", strtotime('last day of this year')); ?> - <?php echo date("F j, Y"); ?></span> <b class="caret"></b>
+        <span><?php echo 'January '.date("1, Y", strtotime('first day of this year')); ?> - <?php echo date("F j, Y"); ?></span> <b class="caret"></b>
       </div>
     </div>
   </div>
@@ -101,5 +95,5 @@ function(start, end) {
 
 </div>
 
-<input class="hide" type="text" id="fro" ng-model="filter.from" />    <!-- {{ filter.from }} --> <br/>
-<input class="hide" type="text" id="to"  ng-model="filter.to"/>   <!-- {{ filter.to }} -->
+<input class="hide" type="text" id="fro" ng-model="filters.startDate" />    <!-- {{ filters.startDate}} --> <br/>
+<input class="hide" type="text" id="to"  ng-model="filters.endDate"/>   <!-- {{ filters.endDate }} -->
--- scripts/controllers/filtersCtrl.js
+++ scripts/controllers/filtersCtrl.js
@@ -1,62 +1,69 @@
 angular
 .module('dashboard')
 .controller('filtersCtrl',['$scope','$http',function($scope,$http){
+
 	$scope.address = {};
-	$scope.refreshAddresses = function(address) {
-		var params = {address: address, sensor: false};
-		return $http.get(
-			'http://maps.googleapis.com/maps/api/geocode/json',
-			{params: params}
-			).then(function(response) {
-				$scope.addresses = response.data.results
+	$scope.filters = {};
+
+
+	$scope.filters.clear = function() {
+		$scope.filters.entities.selected = undefined;
+		$scope.filters.programs.selected = undefined;
+	};
+
+	$scope.filters.programs = 
+	[
+	{ name: 'Viral Load', 				initials:'VL' },
+	{ name: 'Early Infant Diagnosis', 	initials:'EID' },
+	];
+
+	$scope.filters.entities = 
+	[
+	{ name: 'Iringa',      						      					phone:'1234567'		, type: 'Region' },
+	{ name: 'Ruvuma',    				email: 'ruvuma@email.com',    	phone:'1234567'		, type: 'Region' },
+	{ name: 'CSSC', 					email: 'cssc@email.com', 		phone:'1234567'		, type: 'Implementing Partner' },
+	{ name: 'Walter Reed',				email: 'adrian@email.com',    	phone:'1234567'		, type: 'Implementing Partner' },
+	{ name: 'Arumeru',  				email: 'arumeru@email.com',  	phone:'1234567'		, type: 'District' },
+	{ name: 'Kilolo',  					email: 'Kilolo@email.com',  	phone:'1234567'		, type: 'District' },
+	{ name: 'Idodo',  					email: 'Idodo@email.com',  		phone:'1234567'		, type: 'Facility' },
+	{ name: 'Mafinga',    				email: 'Mafinga@email.com',    	phone:'1234567'		, type: 'District' },
+	{ name: 'Mbeya District Hospital', 	email: 'natasha@email.com',   	phone:'1234567'		, type: 'HPV Lab' },
+	{ name: 'Ruvuma Hospital',   		email: 'natasha@email.com',   	phone:'1234567'		, type: 'HPV Lab' },
+	{ name: 'Mgololo',  				email: 'Mgololo@email.com',  	phone:'1234567'		, type: 'Facility' }
+	];
+
+	$scope.filters.programs.selected = 		{ name: 'Viral Load', 				initials:'VL' };
+
+	$scope.filters.startDate = moment().startOf('year').format('YYYY-MM-D');
+	$scope.filters.endDate = moment().format('YYYY-MM-D');
+}])
+.filter('entityFilter', function() {
+	return function(items, props) {
+		var out = [];
+
+		if (angular.isArray(items)) {
+			items.forEach(function(item) {
+				var itemMatches = false;
+
+				var keys = Object.keys(props);
+				for (var i = 0; i < keys.length; i++) {
+					var prop = keys[i];
+					var text = props[prop].toLowerCase();
+					if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+						itemMatches = true;
+						break;
+					}
+				}
+
+				if (itemMatches) {
+					out.push(item);
+				}
 			});
-		};
-		$scope.clear = function() {
-			$scope.person.selected = undefined;
-			$scope.address.selected = undefined;
-			$scope.country.selected = undefined;
-		};
-		$scope.filterentities = [
-		{ name: 'Iringa',      						      		phone:'1234567'		, type: 'Region' },
-		{ name: 'Ruvuma',    	email: 'ruvuma@email.com',    	phone:'1234567'		, type: 'Region' },
-		{ name: 'CSSC', 		email: 'cssc@email.com', 		phone:'1234567'		, type: 'Implementing Partner' },
-		{ name: 'Walter Reed',	email: 'adrian@email.com',    	phone:'1234567'		, type: 'Implementing Partner' },
-		{ name: 'Arumeru',  	email: 'arumeru@email.com',  	phone:'1234567'		, type: 'District' },
-		{ name: 'Kilolo',  	email: 'Kilolo@email.com',  	phone:'1234567'		, type: 'District' },
-		{ name: 'Idodo',  		email: 'Idodo@email.com',  		phone:'1234567'		, type: 'Facility' },
-		{ name: 'Mafinga',    email: 'Mafinga@email.com',    phone:'1234567'		, type: 'District' },
-		{ name: 'Mbeya District Hospital',   email: 'natasha@email.com',   phone:'1234567'		, type: 'HPV Lab' },
-		{ name: 'Ruvuma Hospital',   email: 'natasha@email.com',   phone:'1234567'		, type: 'HPV Lab' },
-		{ name: 'Mgololo',  		email: 'Mgololo@email.com',  		phone:'1234567'		, type: 'Facility' }
-		];
-	}])
-.filter('propsFilter', function() {
-  return function(items, props) {
-    var out = [];
-
-    if (angular.isArray(items)) {
-      items.forEach(function(item) {
-        var itemMatches = false;
-
-        var keys = Object.keys(props);
-        for (var i = 0; i < keys.length; i++) {
-          var prop = keys[i];
-          var text = props[prop].toLowerCase();
-          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-            itemMatches = true;
-            break;
-          }
-        }
-
-        if (itemMatches) {
-          out.push(item);
-        }
-      });
-    } else {
+		} else {
       // Let the output be the input untouched
       out = items;
-    }
+  }
 
-    return out;
-  };
+  return out;
+};
 });
