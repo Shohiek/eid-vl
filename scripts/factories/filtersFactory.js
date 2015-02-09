e8fa9a7453eae412be70902b7effcac68023bca5
MwangiKevin <mwangikevinn@gmail.com>
2015-02-09 12:43:08 +0300

    designed Filters factory and its template api

--- scripts/factories/filtersFactory.js
+++ scripts/factories/filtersFactory.js
@@ -1,4 +1,30 @@
-app.factory('filtersFactory',['$http','$scope', function(
+app.factory('Filters',['$http', function($http){
 
+	var filters = {};
+	filters.getEntities = function(){
+		return $http.get('api/filters/entities').then(function(response){
+			return response.data;
+		});
+	};
+	filters.getPrograms = function(){
+		return $http.get('api/filters/programs').then(function(response){
+			return response.data;
+		});
+	};
 
-	)]);
+	filters.getDates = function(){
+		return $http.get('api/filters/dates').then(function(response){
+			return response.data;
+		});
+	};
+
+	filters.entities 	= getEntities();
+	filters.programs 	= getPrograms();
+	filters.dates 		= getDates();
+
+	filters.entities.selected = undefined;
+	filters.programs.selected = undefined;
+
+	return filters;
+
+}]);
